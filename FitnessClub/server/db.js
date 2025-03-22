af22f6d23360c88e2ab68288375cb41ee863309c
Ranjith S <ranjithselvaraj04@gmail.com>
2025-03-22 22:46:32 +0530

    New Repo Added

--- FitnessClub/server/db.js
+++ FitnessClub/server/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const mongoDb = async () => {
     try {
         const conn = await mongoose.connect('mongodb+srv://devaharsar2004:1234@healthblog.xyuvu.mongodb.net/blog_project?retryWrites=true&w=majority&appName=HealthBlog')
-        console.log("MongoDb Connected")
+        console.log("MongoDb Connected Successfully")
     } catch (error) {
         console.log(error)
     }
