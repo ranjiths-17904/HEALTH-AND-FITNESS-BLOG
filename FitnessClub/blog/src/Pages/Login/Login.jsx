27bdf864df02c56eddf5d17ca487ee27509731da
Ranjith S <ranjithselvaraj04@gmail.com>
2025-07-16 17:46:49 +0530

    Updated in Login

--- FitnessClub/blog/src/Pages/Login/Login.jsx
+++ FitnessClub/blog/src/Pages/Login/Login.jsx
@@ -1,65 +1,10 @@
-import React from 'react'
 import { useState } from "react";
 import { motion } from "framer-motion";
 import { FaUser, FaEnvelope, FaLock, FaSignInAlt, FaUserPlus } from "react-icons/fa";
-import image from '../../assets/Card/loginimg.jpeg';
-import { Link,  useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import image from '../../assets/Card/Login Img.jpg';
 
-const Login = () => {
-    
-    const [email, setEmail] = useState(""); 
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState(null);
-
-    const navigate = useNavigate();
-
-    const back = import.meta.env.VITE_BACKEND_URL
-
-    const handleLogin = async (e) => {
-        e.preventDefault();
-
-        if(!email)
-          {
-            setError("Please enter a valid Email address.");
-            return;
-          }
-      
-          if(!password)
-          {
-            setError("Please enter the password.");
-            return;
-          }
-      
-          setError("")
-
-        //Login API
-        try {
-          const response = await axios.post((back + '/login'),{
-            email : email,
-            password : password,
-          })
-      
-          //Handle successful login
-          if(response.data){
-            navigate("/");
-            console.log("Successfully Logged in")
-            
-          }
-
-        } 
-        catch (error) {
-          //Handle Login error
-          if(error.response && error.response.data && error.response.data.message)
-          {
-            setError(error.response.data.message);
-          }
-          else{
-            setError("An unexpected Error occurs. Please try again.")
-          }
-        }
-        
-    }
+const SignUpPage = () => {
+  const [isLogin, setIsLogin] = useState(true); 
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-900">
@@ -88,16 +33,41 @@ const Login = () => {
           >
             <div className="text-center mb-6">
               <h1 className="text-3xl font-bold text-gray-800 flex items-center justify-center gap-2">
+                {isLogin ? (
+                  <>
                     <FaSignInAlt />
                     Welcome Back!
+                  </>
+                ) : (
+                  <>
+                    <FaUserPlus />
+                    Create an Account
+                  </>
+                )}
               </h1>
               <p className="text-gray-500">
-                "Join us to start your fitness journey today!"
+                {isLogin
+                  ? "Sign in to continue your fitness journey."
+                  : "Join us to start your fitness journey today!"}
               </p>
             </div>
 
-            <form className="space-y-6" onSubmit={handleLogin}>
-              
+            <form className="space-y-6">
+              {!isLogin && (
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">
+                    Full Name
+                  </label>
+                  <div className="relative">
+                    <FaUser className="absolute left-3 top-3 text-gray-400" />
+                    <input
+                      type="text"
+                      placeholder="Your full name"
+                      className="w-full pl-10 px-4 py-2 mt-1 bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500"
+                    />
+                  </div>
+                </div>
+              )}
               <div>
                 <label className="block text-sm font-medium text-gray-700">
                   Email Address
@@ -105,12 +75,9 @@ const Login = () => {
                 <div className="relative">
                   <FaEnvelope className="absolute left-3 top-3 text-gray-400" />
                   <input
-                    type="text"
+                    type="email"
                     placeholder="Your email address"
-                    value={email}
-                    className="w-full pl-10 px-4 py-2 mt-1 bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500" 
-                    onChange={(e) => setEmail(e.target.value)}
-                    />
+                    className="w-full pl-10 px-4 py-2 mt-1 bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500" />
                 </div>
               </div>
               <div>
@@ -121,16 +88,11 @@ const Login = () => {
                   <input
                     type="password"
                     placeholder="Your password"
-                    value={password}
-                    className="w-full pl-10 px-4 py-2 mt-1 bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500"
-                    onChange={(e) => setPassword(e.target.value)}
-                    />
-                    
+                    className="w-full pl-10 px-4 py-2 mt-1 bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500"/>
                 </div>
-                  {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
               </div>
 
-              
+              {isLogin && (
                 <div className="flex items-center justify-between">
                   <label className="flex items-center text-gray-500 text-sm space-x-2">
                     <input type="checkbox" className="form-checkbox" />
@@ -140,34 +102,33 @@ const Login = () => {
                     Forgot Password?
                   </a>
                 </div>
+              )}
 
               <motion.button
                 type="submit"
                 className="w-full py-3 bg-gray-800 hover:bg-gray-900 text-white rounded-lg font-semibold flex items-center justify-center gap-2 transition"
-                onSubmit={handleLogin}
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }} >
-                {<FaSignInAlt />}
-                {"Sign In" }
+                {isLogin ? <FaSignInAlt /> : <FaUserPlus />}
+                {isLogin ? "Sign In" : "Sign Up"}
               </motion.button>
             </form>
 
-            <Link to="/signuppage">
             <div className="mt-6 text-center">
               <p className="text-gray-500">
-                   "Don't have an account?"
-                   {"  "}
-                <span className="text-blue-500 cursor-pointer hover:underline"  >
-                  {"Sign Up"}
+                {isLogin
+                  ? "Don't have an account?"
+                  : "Already have an account?"}{" "}
+                <span className="text-blue-500 cursor-pointer hover:underline" onClick={() => setIsLogin(!isLogin)} >
+                  {isLogin ? "Sign Up" : "Sign In"}
                 </span>
               </p>
             </div>
-            </Link>
           </motion.div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default SignUpPage;
--- FitnessClub/server/index.js
+++ FitnessClub/server/index.js
@@ -1,156 +1,157 @@
+
 const express = require('express');
- const connectDb = require('./db');
- const itemModel = require('./models/item');
- const User = require('./models/user.model');
- const cors = require('cors');
- const bodyParser = require('body-parser');
- 
- const app = express();
- 
- // Middleware
- app.use(bodyParser.json({ limit: '50mb' }));
- app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
- 
- app.use(cors({ origin: 'https://health-and-fitness-blog-serverr.vercel.app/',
-     
- })); 
- 
- // Database Connection
- connectDb();
- 
- // Routes for User
- app.post('/create-account', async (req,res) => {
-     console.log("Server")
-     const {fullName,email,password} = req.body;
- 
-     if(!fullName)
-     {
-         return res.status(400).json({error : true, message : "Full Name is required"})
-     }
- 
-     if(!email)
-     {
-         return res.status(400).json({error : true, message : "Email is required"})
-     }
- 
-     if(!password)
-     {
-         return res.status(400).json({error : true, message : "Password is required"})
-     }
- 
-     const isUser = await User.findOne({email : email})
- 
-     if(isUser)
-     {
-         return res.json({error : true , message : "User already exist"})
-     }
- 
-     const user = new User({
-         fullName,
-         email,
-         password
-     })
- 
-     await user.save();
- 
-     return res.json({
-         error:false,
-         user,
-         message : "Registration Successful",
-     })
- })
- 
- app.post('/login', async (req,res) => {
-     const {email,password} = req.body;
-     console.log("server-side")
-         if(!email)
-         {
-             return res.status(400).json({ message : "Email is required"});
-         }
-         
-         if(!password)
-         {
-             return res.status(400).json({ message : "Password is required"});
-         }
- 
-         const userInfo = await User.findOne({email : email});
- 
-         if(!userInfo)
-         {
-             return res.status(400).json({message: "User not found"});
-         }
- 
-         if(userInfo.email == email && userInfo.password == password)
-             {
-                 return res.json({
-                     error: false,
-                     message : "Login Successful",
-                     email,
-                 });
-             }
-         
-             else
-             {
-                 return res.status(400).json({
-                     error:true,
-                     message:"Invalid Credentials",
-                 });
-             }
- })
- 
- // Routes for Blogs
- app.get('/', async (req, res) => {
-     try {
-         const response = await itemModel.find();
-         return res.json({ items: response });
-     } catch (error) {
-         return res.status(500).json({ error: 'Error fetching items', details: error.message });
-     }
- });
- 
- app.post('/add', async (req, res) => {
-     const { title, description, imageURL } = req.body;
-     if (!title || !description || !imageURL) {
-         return res.status(400).json({ error: 'Title, description, and imageURL are required' });
-     }
- 
-     try {
-         const newItem = new itemModel({ title, description, imageURL });
-         const savedItem = await newItem.save();
-         return res.status(201).json({ message: 'Item added successfully', item: savedItem });
-     } catch (error) {
-         return res.status(400).json({ error: 'Error adding item', details: error.message });
-     }
- });
- 
- 
- app.get('/:id', async (req, res) => {
-     try {
-         const blog = await itemModel.findById(req.params.id);
-         if (!blog) {
-             return res.status(404).json({ error: 'Blog post not found' });
-         }
-         return res.json(blog);
-     } catch (error) {
-         return res.status(500).json({ error: 'Error fetching blog post' });
-     }
- });
- 
- 
- 
- app.delete('/:id', async (req, res) => {
-     try {
-         const deleteItem = await itemModel.findByIdAndDelete(req.params.id);
-         if (!deleteItem) {
-             return res.status(404).json({ error: 'Blog post not found' });
-         }
-         return res.json({ message: 'Blog post deleted successfully', item: deleteItem });
-     } catch (error) {
-         return res.status(500).json({ error: 'Error deleting item', details: error.message });
-     }
- });
- 
- const PORT = process.env.PORT || 3333;
- app.listen(PORT, () => {
-     console.log(`Server is running on port ${PORT}`);
- });
+const connectDb = require('./db');
+const itemModel = require('./models/item');
+const User = require('./models/user.model');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+app.use(cors({ origin: 'http://localhost:5173',
+    
+})); 
+
+// Database Connection
+connectDb();
+
+// Routes for User
+app.post('/create-account', async (req,res) => {
+    console.log("Server")
+    const {fullName,email,password} = req.body;
+
+    if(!fullName)
+    {
+        return res.status(400).json({error : true, message : "Full Name is required"})
+    }
+
+    if(!email)
+    {
+        return res.status(400).json({error : true, message : "Email is required"})
+    }
+
+    if(!password)
+    {
+        return res.status(400).json({error : true, message : "Password is required"})
+    }
+
+    const isUser = await User.findOne({email : email})
+
+    if(isUser)
+    {
+        return res.json({error : true , message : "User already exist"})
+    }
+
+    const user = new User({
+        fullName,
+        email,
+        password
+    })
+
+    await user.save();
+
+    return res.json({
+        error:false,
+        user,
+        message : "Registration Successful",
+    })
+})
+
+app.post('/login', async (req,res) => {
+    const {email,password} = req.body;
+    console.log("server-side")
+        if(!email)
+        {
+            return res.status(400).json({ message : "Email is required"});
+        }
+        
+        if(!password)
+        {
+            return res.status(400).json({ message : "Password is required"});
+        }
+
+        const userInfo = await User.findOne({email : email});
+
+        if(!userInfo)
+        {
+            return res.status(400).json({message: "User not found"});
+        }
+
+        if(userInfo.email == email && userInfo.password == password)
+            {
+                return res.json({
+                    error: false,
+                    message : "Login Successful",
+                    email,
+                });
+            }
+        
+            else
+            {
+                return res.status(400).json({
+                    error:true,
+                    message:"Invalid Credentials",
+                });
+            }
+})
+
+// Routes for Blogs
+app.get('/', async (req, res) => {
+    try {
+        const response = await itemModel.find();
+        return res.json({ items: response });
+    } catch (error) {
+        return res.status(500).json({ error: 'Error fetching items', details: error.message });
+    }
+});
+
+app.post('/add', async (req, res) => {
+    const { title, description, imageURL } = req.body;
+    if (!title || !description || !imageURL) {
+        return res.status(400).json({ error: 'Title, description, and imageURL are required' });
+    }
+
+    try {
+        const newItem = new itemModel({ title, description, imageURL });
+        const savedItem = await newItem.save();
+        return res.status(201).json({ message: 'Item added successfully', item: savedItem });
+    } catch (error) {
+        return res.status(400).json({ error: 'Error adding item', details: error.message });
+    }
+});
+
+
+app.get('/:id', async (req, res) => {
+    try {
+        const blog = await itemModel.findById(req.params.id);
+        if (!blog) {
+            return res.status(404).json({ error: 'Blog post not found' });
+        }
+        return res.json(blog);
+    } catch (error) {
+        return res.status(500).json({ error: 'Error fetching blog post' });
+    }
+});
+
+
+
+app.delete('/:id', async (req, res) => {
+    try {
+        const deleteItem = await itemModel.findByIdAndDelete(req.params.id);
+        if (!deleteItem) {
+            return res.status(404).json({ error: 'Blog post not found' });
+        }
+        return res.json({ message: 'Blog post deleted successfully', item: deleteItem });
+    } catch (error) {
+        return res.status(500).json({ error: 'Error deleting item', details: error.message });
+    }
+});
+
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
